c20403a5325fd6e5d3e877ae9202df1cdffdaf87
Rohit01-zoey <rohitkvartak4@gmail.com>
2021-05-13 20:18:10 +0530

    Update 13/05/2021

--- Scientific Computing/SciML_JuliaCon(2020).jl
+++ Scientific Computing/SciML_JuliaCon(2020).jl
@@ -23,8 +23,42 @@ prob = ODEProblem(latka_votlerra!, u₀, t_span, p)
 
 sol = solve(prob)
 
+using Plots
+plot(sol)
+plot(sol, vars = (1,2))
+
 f(u, p,t) = 1.01*u
 u0 = 1/2
 tspan = (0.0,1.0)
 prob = ODEProblem(f,u0,tspan)
-sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-2)
+sol = solve(prob)
+plot(sol)
+
+#!MY solution whats the error?????????????????????
+α = 1.01
+function linear_de!(du, u,p)
+    r = u
+    du = 1.01*r
+end 
+p = [1.5, 1.0, 3.0, 1.0]
+u₀ = 1/2
+t = (0.0, 1.0)
+prob = ODEProblem(linear_de!, u₀, t)
+
+sol = solve(prob,  Tsit5(), reltol=1e-8, abstol=1e-8)
+
+#*---------------------------------
+function lorentz!(du, u, p ,t)
+    du[1] = 10.0*(u[2] - u[1])
+    du[2] = u[1]*(28.0 - u[3]) - u[2]
+    du[3] = u[1]*u[2] - (8/3)*u[3]
+end
+
+u₀ = [1.0; 0.0; 0.0]
+t_span = (0.0, 100.0)
+prob = ODEProblem(lorentz!, u₀, t_span)
+sol = solve(prob)
+plot(sol)
+plot(sol, vars = (1,2 ,3))
+plot(sol, vars = (1, 3))
+plot(sol, vars = (0, 2))
