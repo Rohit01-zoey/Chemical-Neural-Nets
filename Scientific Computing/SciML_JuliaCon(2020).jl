b7d1257f3ba1a1b01c74880deaa1f033bb35166b
Rohit01-zoey <rohitkvartak4@gmail.com>
2021-05-13 16:50:49 +0530

    update 13/05/2021

--- Scientific Computing/SciML_JuliaCon(2020).jl
+++ Scientific Computing/SciML_JuliaCon(2020).jl
@@ -1,2 +1,30 @@
 #* Univeral Approx. Thm:  Any Neural net can get ϵ close to any polynomial function
+#* SInDy : Sparse Indentification of Dynamical Systems
+#* Discretization
+#* Stiffness of a DE
 
+using DifferentialEquations
+
+#! Lotka Votlerra DE
+
+function latka_votlerra!(du, u, p ,t)
+    r, w = u
+    α, β, γ, δ = p
+    du[1] = dr = α*r - β*r*w
+    du[2] = dw = γ*r*w - δ*w
+end
+
+u₀ = [1.0, 1.0]
+
+t_span = (0.0, 10.0)
+
+p = [1.5, 1.0, 3.0, 1.0]
+prob = ODEProblem(latka_votlerra!, u₀, t_span, p)
+
+sol = solve(prob)
+
+f(u, p,t) = 1.01*u
+u0 = 1/2
+tspan = (0.0,1.0)
+prob = ODEProblem(f,u0,tspan)
+sol = solve(prob, Tsit5(), reltol=1e-8, abstol=1e-2)
