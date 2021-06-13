b98c8d5c4b9f177851c874395b7bce3a09777908
Rohit <rohitkvartak4@gmail.com>
2021-06-13 21:18:37 +0530

    Update 13/06/2021

--- CRNs/intro_to_kinetics.jl
+++ CRNs/intro_to_kinetics.jl
@@ -7,7 +7,7 @@ end α β
 
 using DiffEqBase, OrdinaryDiffEq
 
-p     = [.1/1000, .01]           # [α,β]
+p     = [.1/1000, .01]           # [α,β] i.e the reaction rates.
 tspan = (0.0,250.0)
 u0    = [999.0,1.0,0.0]          # [S,I,R] at t=0
 op    = ODEProblem(rn, u0, tspan, p)
