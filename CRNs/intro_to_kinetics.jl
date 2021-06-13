a341f2f2961fbd20b30cd58656e711a467d7ed34
Rohit <rohitkvartak4@gmail.com>
2021-06-13 21:07:47 +0530

    Update 13/06/2021

---
+++ CRNs/intro_to_kinetics.jl
@@ -0,0 +1,17 @@
+using Catalyst
+
+rn = @reaction_network begin
+    α, S + I --> 2I
+    β, I --> R
+end α β
+
+using DiffEqBase, OrdinaryDiffEq
+
+p     = [.1/1000, .01]           # [α,β]
+tspan = (0.0,250.0)
+u0    = [999.0,1.0,0.0]          # [S,I,R] at t=0
+op    = ODEProblem(rn, u0, tspan, p)
+sol   = solve(op, Tsit5())       # use Tsit5 ODE solver
+
+using Plots
+plot(sol, lw=2)
